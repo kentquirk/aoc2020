1adc5707eda150d60d3c8817b38922e28bd331df
Kent Quirk <kentquirk@gmail.com>
2020-12-14 12:17:36 -0500

    Solve part B

--- day14/main.go
+++ day14/main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-func parseMask(mask string) (int64, int64) {
+func parseMaskA(mask string) (int64, int64) {
 	var zerosMask, onesMask int64
 	zerosMask = 0xffffffff
 	bit := int64(1 << 35)
@@ -27,7 +27,7 @@ func parseMask(mask string) (int64, int64) {
 	return zerosMask, onesMask
 }
 
-func doAll(lines []string) int64 {
+func day14a(lines []string) int64 {
 	pat := regexp.MustCompile("(mask|mem)([^= ]*)[ =]+(.+)")
 	var zerosMask, onesMask int64
 	memory := make(map[int]int64)
@@ -38,7 +38,7 @@ func doAll(lines []string) int64 {
 		}
 		switch groups[1] {
 		case "mask":
-			zerosMask, onesMask = parseMask(groups[3])
+			zerosMask, onesMask = parseMaskA(groups[3])
 		case "mem":
 			addr, _ := strconv.Atoi(groups[2][1 : len(groups[2])-1])
 			writeVal, _ := strconv.ParseInt(groups[3], 10, 64)
@@ -56,6 +56,69 @@ func doAll(lines []string) int64 {
 	return total
 }
 
+func parseMaskB(mask string) (int64, []int64) {
+	var onesMask int64
+	xs := make([]int64, 0)
+	bit := int64(1 << 35)
+	for _, c := range mask {
+		switch c {
+		case '0':
+		case '1':
+			onesMask |= bit
+		case 'X':
+			xs = append(xs, bit)
+		}
+		bit >>= 1
+	}
+
+	addrMasks := make([]int64, 1<<len(xs))
+	for i := range addrMasks {
+		for m := 0; m < len(xs); m++ {
+			bit := 1 << m
+			if i&bit != 0 {
+				addrMasks[i] |= xs[m]
+			}
+		}
+	}
+
+	// fmt.Println(onesMask, xs, addrMasks)
+	return onesMask, addrMasks
+}
+
+func day14b(lines []string) int64 {
+	pat := regexp.MustCompile("(mask|mem)([^= ]*)[ =]+(.+)")
+	var onesMask int64
+	var addrMasks []int64
+	memory := make(map[int64]int64)
+	for _, line := range lines {
+		groups := pat.FindStringSubmatch(line)
+		if len(groups) == 0 {
+			log.Fatalf("no groups found on %s\n", line)
+		}
+		switch groups[1] {
+		case "mask":
+			onesMask, addrMasks = parseMaskB(groups[3])
+		case "mem":
+			addr, _ := strconv.ParseInt(groups[2][1:len(groups[2])-1], 10, 64)
+			writeVal, _ := strconv.ParseInt(groups[3], 10, 64)
+			addr |= onesMask
+			addr &= ^addrMasks[len(addrMasks)-1]
+			for _, m := range addrMasks {
+				a := addr | m
+				memory[a] = writeVal
+			}
+		default:
+			log.Fatalf("unparseable command %s", line)
+		}
+	}
+
+	var total int64
+	for _, v := range memory {
+		total += v
+	}
+	return total
+}
+
 func main() {
 	f, err := os.Open("./input.txt")
 	if err != nil {
@@ -66,5 +129,6 @@ func main() {
 		log.Fatal(err)
 	}
 	lines := strings.Split(string(b), "\n")
-	fmt.Println(doAll(lines))
+	fmt.Println(day14a(lines))
+	fmt.Println(day14b(lines))
 }
